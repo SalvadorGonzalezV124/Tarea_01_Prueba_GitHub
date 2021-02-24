4e2b2708d5a39aab4e8dd09044d939851271f091
SalvadorGonzalezV124 <79558604+SalvadorGonzalezV124@users.noreply.github.com>
2021-02-23 22:22:21 -0600

    Tareas

---
+++ HolaMundo_Gonzalez villasna Salvador.cpp
@@ -0,0 +1,9 @@
+/**
+Primer programa de "hola mundo"
+Gonzalez villasana Salvador
+2EV9
+**/
+printf("programa que ejemplifica un hola mundo");
+printf("Hola <Gonzalez villasana Salvador>");
+getch;
+return 0;
