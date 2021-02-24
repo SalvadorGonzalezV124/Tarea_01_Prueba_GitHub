73d75ddaf945878a91674b954d1147e3f18eb0cc
SalvadorGonzalezV124 <79558604+SalvadorGonzalezV124@users.noreply.github.com>
2021-02-23 22:27:25 -0600

    Delete HolaMundo_Gonzalez villasna Salvador.cpp

--- HolaMundo_Gonzalez villasna Salvador.cpp
+++
@@ -1,9 +0,0 @@
-/**
-Primer programa de "hola mundo"
-Gonzalez villasana Salvador
-2EV9
-**/
-printf("programa que ejemplifica un hola mundo");
-printf("Hola <Gonzalez villasana Salvador>");
-getch;
-return 0;
