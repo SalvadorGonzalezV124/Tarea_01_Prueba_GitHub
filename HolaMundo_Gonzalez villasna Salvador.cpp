ed9c0a1aec505deaf477861bba92d96be1e22eed
SalvadorGonzalezV124 <79558604+SalvadorGonzalezV124@users.noreply.github.com>
2021-02-23 22:28:28 -0600

    tarea 1

---
+++ HolaMundo_Gonzalez villasna Salvador.cpp
@@ -0,0 +1,16 @@
+/**
+Primer programa de "hola mundo"
+Gonzalez villasana Salvador
+2EV9
+**/
+//definicion de archivos de cabecera
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+printf("programa que ejemplifica un hola mundo\n\n");
+printf("Hola <Gonzalez villasana Salvador>");
+getch();
+return 0;
+}
+
